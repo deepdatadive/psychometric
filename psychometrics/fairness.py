b4851a5b8af4452e92615d713a3a25a25d5fb4fc
cfoster <chris.c.foster@gmail.com>
2020-05-08 11:28:51 -0600

    mantel_hanzel, adaptive_testing

--- psychometrics/fairness.py
+++ psychometrics/fairness.py
@@ -1,14 +1,16 @@
 import pandas as pd
 from scipy.stats import chi2_contingency
+import pandas as pd
+import pylab as pl
+import numpy as np
+import statsmodels.api as sm
 
-#todo DIF approaches - , logistic regression, IRT
-
-
+#todo DIF approaches - IRT
 
 def mantel_hanzel_dif(items, response_column='response', refrence_column='group'):
-    items = items['item'].unique().tolist()
+    item_list = items['item'].unique().tolist()
     dict_list = []
-    for item in items:
+    for item in item_list:
         df_item = items[items['item']==item]
         crosstab = pd.crosstab(df_item[response_column], df_item[refrence_column])
         chi2, p, dof, ex = chi2_contingency(crosstab)
@@ -24,3 +26,25 @@ def mantel_hanzel_dif(items, response_column='response', refrence_column='group'
     chi2_df = pd.DataFrame(dict_list)
     return chi2_df
 
+def logistic_regression_dif(items, response_column='response', refrence_column='group'):
+    items_list = items['item'].unique().tolist()
+    dict_list = []
+    for item in items_list:
+        df_item = items[items['item']==item]
+        logit = sm.Logit(df_item[response_column], df_item[refrence_column])
+        result = logit.fit()
+
+        stats_dict = {
+            'item':item,
+            'coef':result.params[0],
+            'p_value':result.pvalues[0],
+            'prquared': result.prsquared
+        }
+        dict_list.append(stats_dict)
+
+    logit_df = pd.DataFrame(dict_list)
+    return logit_df
+
+df = pd.read_csv('/home/cfoster/Documents/dif_CTT.csv')
+result = logistic_regression_dif(df)
+print(result)
